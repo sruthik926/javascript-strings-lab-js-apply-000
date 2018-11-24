bbd29ccea7eebc5971c617e9b20a77323f1a1586
Sruthi Krishna <sruthik926@gmail.com>
2018-11-24 16:19:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = greeting + topic + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "";
 
